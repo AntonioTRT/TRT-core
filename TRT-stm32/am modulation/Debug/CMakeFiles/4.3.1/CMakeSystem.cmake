325043668d2a0fef77427692ff8fdf99fba491ee
AntonioTRT <antonio__estrada@hotmail.com>
2026-09-22 20:51:30 +0300

    update

--- TRT-stm32/am modulation/Debug/CMakeFiles/4.3.1/CMakeSystem.cmake
+++ TRT-stm32/am modulation/Debug/CMakeFiles/4.3.1/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Windows")
 set(CMAKE_HOST_SYSTEM_VERSION "10.0.26200")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
 
-include("C:/Users/anton/STM32CubeIDE/new/trt01/cubeide-gcc.cmake")
+include("D:/repos/TRT-core/TRT-stm32/am modulation/cubeide-gcc.cmake")
 
 set(CMAKE_SYSTEM "Generic")
 set(CMAKE_SYSTEM_NAME "Generic")
